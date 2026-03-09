f97bfaf2e55057b2b06bc60b3b9124dac8ce53e2
Yeison9713 <Yeison9713@gmail.com>
2026-03-09 16:33:12 -0500

    fix: base name of the App

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route index element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
